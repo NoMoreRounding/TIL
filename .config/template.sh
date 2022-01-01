4988fa413906cc9ba0517872bcedeece2f9007b2
CharliePark <37709271+charlieppark@users.noreply.github.com>
2022-01-01 22:35:12 +0900

    Update template.sh

--- .config/template.sh
+++ .config/template.sh
@@ -7,7 +7,7 @@ TEMPLATE_FILE="${TEMPLATE_DIR}/TIL_TEMPLATE.md"
 
 mkdir ${TEMPLATE_DIR}
 touch ${TEMPLATE_FILE}
-echo "![header](https://capsule-render.vercel.app/api?type=soft&color=F2F5A9&height=150&section=header&text=T&nbsp;I&nbsp;L&fontSize=100&animation=blink)" >> ${TEMPLATE_FILE}
+echo "![header](https://capsule-render.vercel.app/api?type=soft&color=084B8A&height=20&section=header)" >> ${TEMPLATE_FILE}
 echo ""
 echo "# Today I Learned" >> ${TEMPLATE_FILE}
 echo "" >> ${TEMPLATE_FILE}
@@ -27,4 +27,4 @@ echo "### Retrospective" >> ${TEMPLATE_FILE}
 echo "" >> ${TEMPLATE_FILE}
 echo "Hello World!" >> ${TEMPLATE_FILE}
 echo "" >> ${TEMPLATE_FILE}
-echo "![footer](https://capsule-render.vercel.app/api?type=soft&color=F2F5A9&height=150&section=footer)" >> ${TEMPLATE_FILE}
+echo "![footer](https://capsule-render.vercel.app/api?type=soft&color=084B8A&height=20&section=footer)" >> ${TEMPLATE_FILE}
